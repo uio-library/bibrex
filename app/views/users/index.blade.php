696044518c6f67c6014f85db32b656a071718211
Dan Michael O. Heggø <danmichaelo@gmail.com>
2013-08-24 20:23:28 +0000

    Add checkbox to toggle display of only users with loans

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -8,10 +8,15 @@
       <h3 class="panel-title">Brukere</h3>
     </div>
 
+    <p>
+      <input type="checkbox" id="onlyUsersWithLoans">
+      <label for="onlyUsersWithLoans">Vis bare folk med aktive lån</label>
+    </p>
+
     <!-- List group -->
     <ul class="list-group">
       @foreach ($users as $user)
-        <li class="list-group-item">
+        <li class="list-group-item" data-loanscount="{{ $user->loans->count() }}">
           <span class="badge">{{ $user->loans->count() }}</span>
           <a href="{{ URL::action('UsersController@getShow', $user->id) }}">{{ $user->lastname }}, {{ $user->firstname }}</a>
         </li>
@@ -25,8 +30,21 @@
 
 @section('scripts')
 
-<script type='text/javascript'>     
+<script type='text/javascript'>
   $(document).ready(function() {
+
+    $('#onlyUsersWithLoans').on('change', function() {
+      var onlyUsersWithLoans = $('#onlyUsersWithLoans').is(':checked');
+      $('.list-group-item').each(function(key, elem) {
+        if (!onlyUsersWithLoans || $(elem).data('loanscount') > 0) {
+          $(elem).show();
+        } else {
+          $(elem).hide();
+        }
+      });
+
+    });
+
 
   });
 </script>
