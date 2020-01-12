b2c3eb29b9763e0f96f1709078031cff4ecd37b9
Dan Michael O. Heggø <danmichaelo@gmail.com>
2020-01-12 21:56:30 +0100

    Remove static call to Monolog::setTimezone
    No longer supported. Not sure if we really need to set Monolog's
    timezone directly. Let's try to remove it.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \Monolog\Logger::setTimezone(new \DateTimeZone('Europe/Oslo'));
+        //
     }
 
     /**
